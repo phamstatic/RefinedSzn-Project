77def6ca26e30d50e8f738f7fb831052610662e1
Phamstatic <phambinouh@gmail.com>
2022-06-26 01:23:28 -0500

    Added a line break in the information container description.

--- frontend/src/components/Information.js
+++ frontend/src/components/Information.js
@@ -6,7 +6,10 @@ const Information = () => {
             <div class="MainContainer row">
                 <div className="InfoLeft col-4 d-flex">
                     <p className="InfoLeft"><strong>RefinedSzn</strong>
-                    <p className="">We're a group of automotive enthusiast friends who came together to create a team of unique car builds. We love to hang out on the weekends and mob together to car meets whenever one pops up. Our squad is very well known in the streets of Houston's Chinatown.</p>
+                    <p>We're a group of automotive enthusiast friends who came together to create a team of unique car builds. We love to hang out on the weekends and mob together to car meets whenever one pops up.
+                    <p></p>
+                    <p>Our squad is very well known in the streets of Houston's Chinatown.</p>
+                    </p>
                     </p>
                 </div>
                 <div className="col-8 d-flex justify-content-center">
