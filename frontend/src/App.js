37975e0a759662dbf62d6f12700d0c881873631b
Phamstatic <phambinouh@gmail.com>
2022-06-25 23:55:55 -0500

    Moved the navigation bar from the App.js file to its own component and export.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,13 @@
 import './App.css';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import Disclaimer from './components/Disclaimer';
-import TopHeader from './components/TopHeader';
+import TopNavigation from './components/TopNavigation';
 import Gallery from './components/Gallery';
 function App() {
   return (
     <>
       <Disclaimer />
-      <div className="NavigationBar navbar-dark">
-        <Navbar bg="black"
-        sticky="top" 
-        expand="md"
-        collapseOnSelect>
-          <Navbar.Brand>
-            <img src="https://cdn.discordapp.com/attachments/978388869131165736/987971273500004382/RefinedW.png"
-              className="RefinedLogo d-block w-50 mx-auto"
-              alt="Refined Logo"/>
-          </Navbar.Brand>
-
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav>
-              <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Home</Nav.Link>
-              <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">About Us</Nav.Link>
-              <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Team Roster</Nav.Link>
-              <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Contact</Nav.Link>
-              <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Account</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-
-        </Navbar>
-      </div>
-  
+      <TopNavigation />
       <Gallery />
     </>
   );
---
+++ frontend/src/components/TopNavigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+const TopNavigation = () => {
+    return (
+        <>
+            <div className="NavigationBar navbar-dark">
+                <Navbar bg="black"
+                    sticky="top"
+                    expand="md"
+                    collapseOnSelect>
+                    <Navbar.Brand>
+                        <img src="https://cdn.discordapp.com/attachments/978388869131165736/987971273500004382/RefinedW.png"
+                            className="RefinedLogo d-block w-50 mx-auto"
+                            alt="Refined Logo" />
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle />
+                    <Navbar.Collapse>
+                        <Nav>
+                            <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Home</Nav.Link>
+                            <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">About Us</Nav.Link>
+                            <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Team Roster</Nav.Link>
+                            <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Contact</Nav.Link>
+                            <Nav.Link className="NavButton px-5 text-white d-flex align-items-center" href="">Account</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Navbar>
+            </div>
+        </>
+    );
+}
+
+export default TopNavigation;
