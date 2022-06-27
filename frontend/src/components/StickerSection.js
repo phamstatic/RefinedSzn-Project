f7b42c14bfd2cb5e56d1cd10a030f5480dfa7101
Phamstatic <phambinouh@gmail.com>
2022-06-27 01:07:15 -0500

    Fixed typos and updated prices on stickers.

--- frontend/src/components/StickerSection.js
+++ frontend/src/components/StickerSection.js
@@ -19,7 +19,7 @@ const StickerSection = () => {
                         <figure className="">
                             <img className="d-block w-75 p-3 mx-auto"
                             src="https://cdn.discordapp.com/attachments/989767720801304606/990852464456634379/unknown.png"/>
-                            <figcaption className="StickerCaption d-flex justify-content-center">Refined Windshield Banner 12"</figcaption>
+                            <figcaption className="StickerCaption d-flex justify-content-center">Refined Mini Sticker 5"</figcaption>
                             <figcaption className="StickerCaption d-flex justify-content-center">$5.00</figcaption>
                         </figure>
                     </div>
@@ -27,7 +27,7 @@ const StickerSection = () => {
                         <figure className="">
                             <img className="d-block w-75 p-3 mx-auto"
                             src="https://cdn.discordapp.com/attachments/989767720801304606/990851925425651732/unknown.png"/>
-                            <figcaption className="StickerCaption d-flex justify-content-center">Refined Windshield Banner 12"</figcaption>
+                            <figcaption className="StickerCaption d-flex justify-content-center">Refined Off-White Style Sticker 5"</figcaption>
                             <figcaption className="StickerCaption d-flex justify-content-center">$5.00</figcaption>
                         </figure>
                     </div>
