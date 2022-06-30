362c15f136d64867efa013c07091379e594dd253
Phamstatic <phambinouh@gmail.com>
2022-06-30 03:09:10 -0500

    Disabled HashRouter.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { HashRouter } from 'react-router-dom';
+//import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
