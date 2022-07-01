3dc78b2af8a7d25d890490311a3d7c621b9a06eb
Phamstatic <phambinouh@gmail.com>
2022-07-01 00:35:23 -0500

    Added a picture to the error page.

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react"
+import '../App.css';
 
-function Error() {
-    return <div>error</div>;
+import TopNavigation from "../components/TopNavigation";
+import Footer from "../components/Footer";
+
+const Error = () => {
+    return (
+        <>
+            <TopNavigation />
+            <img
+                className="d-block w-100"
+                src="https://cdn.discordapp.com/attachments/989767720801304606/992295833078865970/unknown.png"
+            />
+            <Footer />
+        </>
+    );
 }
+
 
 export default Error;
