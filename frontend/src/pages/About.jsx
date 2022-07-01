2aac09637ea7de33eb0ea167196bcedf43aba74e
Phamstatic <phambinouh@gmail.com>
2022-07-01 00:35:45 -0500

    Adding code for the About Us page.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,33 @@
 import React from "react"
-
 import '../App.css';
 
+import TopNavigation from "../components/TopNavigation";
+import Footer from "../components/Footer";
+
 const About = () => {
-    return (   
+    return (
         <>
-        <div>About Us Page!</div>
+            <TopNavigation />
+
+            <img
+                className="d-block w-100"
+                src="https://cdn.discordapp.com/attachments/989767720801304606/992297000416587866/unknown.png"
+            />
+            <div className="container-fluid">
+                <div className="AboutColumn col-4">
+                    <div className="AboutTitleTop row align-items-center justify-content-left">OUR TEAM</div>
+                    <div className="AboutTitle row align-items-center justify-content-left">RefinedSzn</div>
+                    <div className="AboutInfo row align-items-center justify-content-left">Founded in December of 2020, RefinedSzn started as a group of automotive enthusiast friends who came together to create a team of unique car builds. Although we've had members come and leave, what is left behind is a true unbreakable family.</div>
+                    <div className="AboutInfo row align-items-center justify-content-left"><br></br></div>
+                    <div className="AboutInfo row align-items-center justify-content-left">Founded in December of 2020, RefinedSzn started as a group of automotive enthusiast friends who came together to create a team of unique car builds. Although we've had members come and leave, what is left behind is a true unbreakable family.</div>
+                    <div className="AboutInfo row align-items-center justify-content-left"><br></br></div>
+                    <div className="AboutInfo row align-items-center justify-content-left">#ComeBackSzn</div>
+                </div>
+
+                <div className="col-8"></div>
+            </div>
+
+            <Footer />
         </>
     );
 }
