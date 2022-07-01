97133e0b55c7e1f08dd648f640ba2f098905ef0d
Phamstatic <phambinouh@gmail.com>
2022-06-30 23:59:37 -0500

    Recreated the home page.

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import '../App.css';
+
+import Disclaimer from '../components/Disclaimer';
+import TopNavigation from '../components/TopNavigation';
+import Gallery from '../components/Gallery';
+import Information from '../components/Information';
+import StickerSection from '../components/StickerSection';
+import Footer from '../components/Footer';
+
+const Home = () => {
+    return (   
+        <>
+        <Disclaimer />
+        <TopNavigation />
+        <Gallery />
+        <Information />
+        <StickerSection />
+        <Footer />
+        </>
+    );
+}
+
+export default Home;
