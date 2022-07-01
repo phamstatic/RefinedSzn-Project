1786210d51b8dfc2aac7205e8eb17747074cc8b8
Phamstatic <phambinouh@gmail.com>
2022-06-30 23:59:55 -0500

    Successfully implemented Hash Router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react"
 import './App.css';
 
-
 import Disclaimer from './components/Disclaimer';
 import TopNavigation from './components/TopNavigation';
 import Gallery from './components/Gallery';
@@ -9,16 +8,19 @@ import Information from './components/Information';
 import StickerSection from './components/StickerSection';
 import Footer from './components/Footer';
 
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
 function App() {
   return (
-    <>
-    <Disclaimer />
-    <TopNavigation />
-    <Gallery />
-    <Information />
-    <StickerSection />
-    <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="*" element={<Error />}/>
+      </Routes>
+    </Router>
   );
 }
 
