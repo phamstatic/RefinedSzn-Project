b0748442cdae61737d2d095fedfb341ec7629885
Phamstatic <phambinouh@gmail.com>
2022-07-01 00:52:29 -0500

    Finished majority of the About Us page.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -14,17 +14,24 @@ const About = () => {
                 src="https://cdn.discordapp.com/attachments/989767720801304606/992297000416587866/unknown.png"
             />
             <div className="container-fluid">
-                <div className="AboutColumn col-4">
-                    <div className="AboutTitleTop row align-items-center justify-content-left">OUR TEAM</div>
-                    <div className="AboutTitle row align-items-center justify-content-left">RefinedSzn</div>
-                    <div className="AboutInfo row align-items-center justify-content-left">Founded in December of 2020, RefinedSzn started as a group of automotive enthusiast friends who came together to create a team of unique car builds. Although we've had members come and leave, what is left behind is a true unbreakable family.</div>
-                    <div className="AboutInfo row align-items-center justify-content-left"><br></br></div>
-                    <div className="AboutInfo row align-items-center justify-content-left">Founded in December of 2020, RefinedSzn started as a group of automotive enthusiast friends who came together to create a team of unique car builds. Although we've had members come and leave, what is left behind is a true unbreakable family.</div>
-                    <div className="AboutInfo row align-items-center justify-content-left"><br></br></div>
-                    <div className="AboutInfo row align-items-center justify-content-left">#ComeBackSzn</div>
-                </div>
+                <div className="AboutRow row">
+                    <div className="AboutColumn col-4">
+                        <div className="AboutTitleTop row align-items-center justify-content-left">OUR TEAM</div>
+                        <div className="AboutTitle row align-items-center justify-content-left">RefinedSzn</div>
+                        <div className="AboutInfo row align-items-center justify-content-left">Founded in December of 2020, RefinedSzn started as a group of automotive enthusiast friends who came together to create a team of unique car builds. Although we've had members come and leave, what is left behind is a true unbreakable family.</div>
+                        <div className="AboutInfo row align-items-center justify-content-left"><br></br></div>
+                        <div className="AboutInfo row align-items-center justify-content-left">Founded in December of 2020, RefinedSzn started as a group of automotive enthusiast friends who came together to create a team of unique car builds. Although we've had members come and leave, what is left behind is a true unbreakable family.</div>
+                        <div className="AboutInfo row align-items-center justify-content-left"><br></br></div>
+                        <div className="AboutInfo row align-items-center justify-content-left">#ComeBackSzn</div>
+                    </div>
 
-                <div className="col-8"></div>
+                    <div className="AboutImage col-8 ">
+                        <img
+                            className="d-block w-100"
+                            src="https://cdn.discordapp.com/attachments/989767720801304606/992303865758236682/unknown.png"
+                        />
+                    </div>
+                </div>
             </div>
 
             <Footer />
