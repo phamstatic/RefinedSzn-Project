916d38fe12e21287c2bdf2db7b1c3344af7d0152
Phamstatic <phambinouh@gmail.com>
2022-07-01 22:40:37 -0500

    Cleaning up unneeded code.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter as Router } from "react-router-dom";
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<Router>
     <App />
-  //</Router>
 
   ,document.getElementById('root')
 );
