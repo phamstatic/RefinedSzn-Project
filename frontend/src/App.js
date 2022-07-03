8464af0e276e04cbd35f5ea2fde2871896e52958
Phamstatic <phambinouh@gmail.com>
2022-07-03 00:34:17 -0500

    Changing website version number.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 //Pages
 import Home from './pages/Home';
 import About from './pages/About';
+
+import Dev from './pages/Dev';
+
 import Error from './pages/Error';
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+
+        <Route path="/Dev" element={<Dev />} />
+
         <Route path="*" element={<Error />}/>
       </Routes>
     </Router>
