4cb1af9dd37be375655f3a72b96aa72075c1a2fd
Phamstatic <phambinouh@gmail.com>
2022-07-03 00:33:18 -0500

    Added a test development page.

---
+++ frontend/src/pages/Dev.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import TopNavigation from "../components/TopNavigation";
+import Footer from "../components/Footer";
+
+const Dev = () => {
+    return (
+        <>
+        <TopNavigation />
+        <Footer />
+        </>
+    );
+}
+
+
+export default Dev;
