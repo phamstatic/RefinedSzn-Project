bc8c708e26f49256650a7533d071d40921ea08da
Phamstatic <phambinouh@gmail.com>
2022-07-03 23:19:37 -0500

    Added a subtitle under the main title.

--- frontend/src/components/StickerSection.js
+++ frontend/src/components/StickerSection.js
@@ -7,11 +7,12 @@ const StickerSection = () => {
             <div className="container-fluid bg-black">
                 <div className="StickerRow row">
                     <div className="StickerHeader">Stickers</div>
+                    <div className="StickerSubHeader"><em>**Currently only available for members to purchase**</em></div>
                 </div>
                 <div className="row align-items-center">
                     <div className="StickerBox col-4">
                         <figure className="">
-                            <img className="d-block w-75 p-3 mx-auto"
+                            <img className="Sticker d-block w-75 p-3 mx-auto"
                             src="https://cdn.discordapp.com/attachments/989767720801304606/990852269895454810/unknown.png"/>
                             <figcaption className="StickerCaption d-flex justify-content-center">Windshield Banner 20"</figcaption>
                             <figcaption className="StickerCaption d-flex justify-content-center">$10.00</figcaption>
@@ -19,7 +20,7 @@ const StickerSection = () => {
                     </div>
                     <div className="StickerBox col-4">
                         <figure className="">
-                            <img className="d-block w-75 p-3 mx-auto"
+                            <img className="Sticker d-block w-75 p-3 mx-auto"
                             src="https://cdn.discordapp.com/attachments/989767720801304606/990852464456634379/unknown.png"/>
                             <figcaption className="StickerCaption d-flex justify-content-center">Mini Sticker 5"</figcaption>
                             <figcaption className="StickerCaption d-flex justify-content-center">$5.00</figcaption>
@@ -27,7 +28,7 @@ const StickerSection = () => {
                     </div>
                     <div className="StickerBox col-4">
                         <figure className="">
-                            <img className="d-block w-75 p-3 mx-auto"
+                            <img className="Sticker d-block w-75 p-3 mx-auto"
                             src="https://cdn.discordapp.com/attachments/989767720801304606/990851925425651732/unknown.png"/>
                             <figcaption className="StickerCaption d-flex justify-content-center">Off-White Style Sticker 5"</figcaption>
                             <figcaption className="StickerCaption d-flex justify-content-center">$5.00</figcaption>
